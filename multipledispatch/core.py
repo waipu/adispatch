c657d6d9f9b5a1a3fa99b6eaaf4beeb7be87889e
waipu <waipu@cirno.de>
2015-04-26 16:37:28 +0300

    Use inspec.getfullargspec instead of inspect.getargspec to support annotated functions.

--- multipledispatch/core.py
+++ multipledispatch/core.py
@@ -76,5 +76,5 @@ def ismethod(func):
     Note that this has to work as the method is defined but before the class is
     defined.  At this stage methods look like functions.
     """
-    spec = inspect.getargspec(func)
+    spec = inspect.getfullargspec(func)
     return spec and spec.args and spec.args[0] == 'self'
